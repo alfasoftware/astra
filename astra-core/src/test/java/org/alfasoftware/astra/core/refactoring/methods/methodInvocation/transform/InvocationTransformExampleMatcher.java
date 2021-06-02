b721e0b31e7c9c955817faeb472814bc0a01fb37
Oyvind Woller <oyvind.woller@alfasystems.com>
2021-06-02 10:03:05 +0200

    Fix for build failure

--- astra-core/src/test/java/org/alfasoftware/astra/core/refactoring/methods/methodInvocation/transform/InvocationTransformExampleMatcher.java
+++ astra-core/src/test/java/org/alfasoftware/astra/core/refactoring/methods/methodInvocation/transform/InvocationTransformExampleMatcher.java
@@ -3,7 +3,8 @@ package org.alfasoftware.astra.core.refactoring.methods.methodInvocation.transfo
 import org.alfasoftware.astra.core.refactoring.operations.javapattern.JavaPattern;
 import org.alfasoftware.astra.core.refactoring.operations.javapattern.JavaPatternReplacement;
 
-class InvocationTransformExampleMatcher {
+// public so that it can be next to the corresponding example
+public class InvocationTransformExampleMatcher {
 
   @JavaPattern
   ReturnsObject matcher(ReturnsObject toReturn){
